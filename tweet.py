fc6b8d54f0ac750fd1ce54eda09e8b4b9c318560
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-09-13 18:41:15 +0100

    Don't reply if there's no function

--- tweet.py
+++ tweet.py
@@ -20,28 +20,29 @@ def get_function(tweet):
     if "f(x)=" in tweet:
         return tweet.split("f(x)=")[1]
     else:
-        return "ERROR"
+        return "NO FUNCTION"
 
 for tweet in query[::-1]:
     if tweet["id"] not in done:
         function = get_function(tweet["text"])
         done.append(tweet["id"])
         user = tweet["user"]["screen_name"]
-        try:
-            f = fp.parse(function)
-            data, rating, n = ip.interpolate_and_rate(f)
-        except TypeError:
-            tweet_this = ".@"+user+" I couldn't understand your function. Sorry."
-            results = twitter.statuses.update(status=tweet_this, in_reply_to_status_id=tweet["id"])
+        if function != "NO FUNCTION":
+            try:
+                f = fp.parse(function)
+                data, rating, n = ip.interpolate_and_rate(f)
+            except TypeError:
+                tweet_this = ".@"+user+" I couldn't understand your function. Sorry."
+                results = twitter.statuses.update(status=tweet_this, in_reply_to_status_id=tweet["id"])
+                break
+
+            tweet_this = ".@"+user+" Here's your function interpolated using "
+            tweet_this+= str(n)+" equally spaced points (blue) and "
+            tweet_this+= str(n)+" Chebyshev points (red). "
+            tweet_this+= "For your function, Runge's phenomenon is "+rating+"."
+
+            results = twitter.statuses.update_with_media(status=tweet_this, media=data, in_reply_to_status_id=tweet["id"])
             break
-
-        tweet_this = ".@"+user+" Here's your function interpolated using "
-        tweet_this+= str(n)+" equally spaced points (blue) and "
-        tweet_this+= str(n)+" Chebyshev points (red). "
-        tweet_this+= "For your function, Runge's phenomenon is "+rating+"."
-
-        results = twitter.statuses.update_with_media(status=tweet_this, media=data, in_reply_to_status_id=tweet["id"])
-        break
 
 with open("done.json","w") as f:
     json.dump(done,f)
