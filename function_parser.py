90a83fff371bd47ab116ebd3f1fb4027006c42ab
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-11-13 15:49:05 +0000

    math.

--- function_parser.py
+++ function_parser.py
@@ -8,6 +8,11 @@ class ParsingError(BaseException):
 
 def parse(func):
     func = func.replace("^", "**")
+
+    for i in ["exp", "sin", "cos", "tan", "log"]:
+        func = func.replace(i, f"math.{i}")
+    func = func.replace("math.math", "math")
+
     func = re.sub(r"([0-9])x", r"\1*x", func)
     parsed = Expression(func, ["x"])
     return parsed
