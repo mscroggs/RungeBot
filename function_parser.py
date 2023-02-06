df170b656f0797a3e856bbe615a0a4351651a28e
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-02-06 20:49:54 +0000

    cos x -> cos(x)

--- function_parser.py
+++ function_parser.py
@@ -18,7 +18,15 @@ def parse(func):
     func = func.replace("sin-1", "arcsin")
     func = func.replace("cos-1", "arccos")
 
+    func = func.replace("cos x", "cos(x)")
+    func = func.replace("sin x", "sin(x)")
+    func = func.replace("tan x", "tan(x)")
+    func = func.replace("sec x", "sec(x)")
+    func = func.replace("cosec x", "cosec(x)")
+    func = func.replace("cot x", "cot(x)")
+
     func = func.replace("ln", "log")
+    func = func.replace("csc", "cosec")
 
     func = re.sub(r"([0-9])([A-Za-z])", r"\1*\2", func)
     func = re.sub(r"\)([A-Za-z])", r")*\1", func)
