a0301d59939d293a07632fda7c997d326a69ea71
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-02-06 20:28:27 +0000

    []

--- function_parser.py
+++ function_parser.py
@@ -11,8 +11,8 @@ def parse(func):
     func = func.replace("exp", "e**")
     func = func.replace(")(", ")*(")
 
-    func = re.sub(r"([0-9])(A-Za-z)", r"\1*\2", func)
-    func = re.sub(r"\)(A-Za-z)", r")*\1", func)
+    func = re.sub(r"([0-9])([A-Za-z])", r"\1*\2", func)
+    func = re.sub(r"\)([A-Za-z])", r")*\1", func)
     func = re.sub(r"-x\*\*([0-9]+)", r"-(x**\1)", func)
 
     print(func)
