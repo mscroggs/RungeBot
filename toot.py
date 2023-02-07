325ca8925eb08cb1f0720268a32389a6c0ce1d7c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-02-07 08:04:09 +0000

    no need to check file for already done

--- toot.py
+++ toot.py
@@ -1,39 +1,37 @@
 from mastodon import Mastodon
 import json
+import sys
 from prepare_tweet import create_tweet
 
-with open("mdone.json") as f:
-    done = json.load(f)
+test = "test" in sys.argv
 
 mdon = Mastodon(
     access_token="mdon.secret", api_base_url="https://mathstodon.xyz")
 
 
-def get_function(toot):
-    toot = "".join(toot.split())
-    if "f(x)=" in toot:
-        return toot.split("f(x)=")[1].split("</p>")[0]
-    else:
-        return None
+def replied(id):
+    for d in mdon.status_context(id):
+        if "account" in d and "username" in d["account"] and d["account"]["username"] == "RungeBot":
+            return True
+    return False
 
 
 for toot in mdon.notifications():
-    if toot["type"] == "mention" and toot["status"]["id"] not in done:
-        done.append(toot["status"]["id"])
+    if toot["type"] == "mention" and  not replied(toot["status"]["id"]):
         user = toot["account"]["acct"]
 
         toot_this, data = create_tweet(
             toot["status"]["content"].replace("</p>", "").replace("<p>", ""),
             user, fname="toot_me")
         if toot_this is not None:
-            if data is None:
-                mdon.status_post(toot_this, in_reply_to_id=toot["status"]["id"])
+            if test:
+                print(toot_this)
             else:
-                img = mdon.media_post("toot_me.png")
-                results = mdon.status_post(
-                    status=toot_this, media_ids=img, in_reply_to_id=toot["status"]["id"])
+                if data is None:
+                    mdon.status_post(toot_this, in_reply_to_id=toot["status"]["id"])
+                else:
+                    img = mdon.media_post("toot_me.png")
+                    results = mdon.status_post(
+                        status=toot_this, media_ids=img, in_reply_to_id=toot["status"]["id"])
             print(toot_this)
             break
-
-with open("mdone.json", "w") as f:
-    json.dump(done, f)
