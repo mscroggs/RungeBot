b32b7fb96dbaa5302771b06242259337420757a2
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-02-07 08:29:09 +0000

    ! and //

--- test/test_functions.py
+++ test/test_functions.py
@@ -8,7 +8,7 @@ import pytest
     "5**x", "1/(1+25*x**2)", "arctan(x)", "arcsin(x)", "arccos(x)",
     # Functions from Twitter/Mathstodon that have previously failed
     "x^x", "1/x", "(25/24)abs(x+(1/7))-(7/24)(x+(1/7))", "2*x + -x^(x-2)",
-    "log(x)", "ln(x)", "cos x"
+    "log(x)", "ln(x)", "cos x", "x!"
 ])
 def test_parsing(function_string):
     f = fp.parse(function_string)
